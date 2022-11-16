aacdc01d0dc44f9d1499652fbb9672ed3c86b7be
h-beeen <hbyeon@student.42seoul.kr>
2022-11-17 06:49:04 +0900

    2022/11/17 16:48

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 03:46:15 by hbyeon            #+#    #+#             */
-/*   Updated: 2022/11/17 05:45:17 by hbyeon           ###   ########.fr       */
+/*   Updated: 2022/11/17 06:38:20 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	unsigned int	s1_len;
 	unsigned int	s2_len;
-	unsigned int	j;
+	unsigned int	a;
 	unsigned int	i;
 	char			*ptr;
 
