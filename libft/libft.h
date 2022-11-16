be3ec00d6b6d836de37f820793934a6c21678031
Haebin <hbyeon@student.42seoul.kr>
2022-11-16 15:18:57 +0900

    2022/11/16 15:18

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hbyeon <hbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 20:27:54 by hbyeon            #+#    #+#             */
-/*   Updated: 2022/11/16 09:03:22 by hbyeon           ###   ########.fr       */
+/*   Updated: 2022/11/16 09:13:36 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,6 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 void	ft_bzero(void *s, size_t n);
 int		ft_atoi(const char *str);
-size_t	ft_strlcpy(char *restrict dest, const char *restrict src, size_t size);
+size_t	ft_strlcat(char *restrict dest, const char *restrict src, size_t size);
 
 #endif
