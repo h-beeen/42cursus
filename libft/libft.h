50d9eedfc19bd9407b33a184fda0e62969ea02fb
h-beeen <hbyeon@student.42seoul.kr>
2022-11-20 02:37:57 +0900

    2022/11/20 02:37

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 20:27:54 by hbyeon            #+#    #+#             */
-/*   Updated: 2022/11/20 00:15:40 by hbyeon           ###   ########.fr       */
+/*   Updated: 2022/11/20 01:42:37 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_itoa(int n);
 
 #endif
