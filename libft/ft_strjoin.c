c4c2e1a2b8e03215f807e0b3f3032784e26dca44
h-beeen <hbyeon@student.42seoul.kr>
2022-11-19 20:04:46 +0900

    2022/11/19 20:04

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 03:46:15 by hbyeon            #+#    #+#             */
-/*   Updated: 2022/11/17 06:38:20 by hbyeon           ###   ########.fr       */
+/*   Updated: 2022/11/19 20:02:12 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	unsigned int	s1_len;
 	unsigned int	s2_len;
-	unsigned int	a;
-	unsigned int	i;
 	char			*ptr;
 
-	i = 0;
-	j = 0;
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
+	if (!s1_len && !s2_len)
+		return (ft_strdup(""));
 	ptr = (char *)malloc ((sizeof(char)) * (s1_len + s2_len + 1));
 	if (!ptr)
 		return (0);
-	while (!s1[i])
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	while (!s2[j])
-	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
-	}
-	ptr[i] = '\0';
+	ft_strlcpy(ptr, s1, s1_len + 1);
+	ft_strlcpy(ptr + s1_len, s2, s2_len + 1);
 	return (ptr);
 }
