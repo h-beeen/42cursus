6a0f1706248182ac1084d088e42778fb7d41e3b7
h-beeen <hbyeon@student.42seoul.kr>
2022-11-20 00:27:20 +0900

    2022/11/20 00:27

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 00:15:02 by hbyeon            #+#    #+#             */
-/*   Updated: 2022/11/20 00:15:27 by hbyeon           ###   ########.fr       */
+/*   Updated: 2022/11/20 00:26:55 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t				i;
+	size_t	i;
 
-	i = 0;
-	while (i < n)
-	{
-		if (((unsigned char *)s)[i] == (unsigned char) c)
-			return ((void *)s + i);
-		i++;
-	}
+	i = -1;
+	while (++i < n)
+		if (((const unsigned char *)s1)[i] != ((const unsigned char *)s2)[i])
+			return (((const unsigned char *)s1)[i] - \
+			((const unsigned char *)s2)[i]);
 	return (0);
 }
