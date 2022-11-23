070f7004229eb6a56a39fddb9a8efa416852e875
Haebin <hbyeon@student.42seoul.kr>
2022-11-23 12:01:40 +0900

    2022/11/23 12:01

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: hbyeon <hbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 16:09:38 by hbyeon            #+#    #+#             */
-/*   Updated: 2022/11/23 11:09:41 by hbyeon           ###   ########.fr       */
+/*   Updated: 2022/11/23 11:51:03 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstadd_front(t_list **lst, t_list *new)
+void	*ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return (0);
