d9f795475c6ea46e9f6e9e620bf7fbe9fd4ce47b
h-beeen <hbyeon@student.42seoul.kr>
2023-01-20 20:14:39 +0900

    2023/01/20 20:14

--- get_next_line/get_next_line/get_next_line.h
+++ get_next_line/get_next_line/get_next_line.h
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbyeon <hbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/07 00:41:32 by gemartin          #+#    #+#             */
-/*   Updated: 2022/12/30 23:06:56 by hbyeon           ###   ########.fr       */
+/*   Created: 2023/01/20 20:14:21 by hbyeon            #+#    #+#             */
+/*   Updated: 2023/01/20 20:14:30 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include<unistd.h>
-# include<stdio.h>
-# include<stdlib.h>
-# include<fcntl.h>
-# include<limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
 
-size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
-size_t	ft_strlen(char *s);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(char *s1);
 char	*ft_strchr(char *s, int c);
 char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(char *s);
 char	*get_next_line(int fd);
 char	*ft_free(char **str);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
 #endif
