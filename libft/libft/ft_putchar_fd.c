4dbc73055d74ef4b7be51f32c0e605537c5d702d
h-beeen <hbyeon@student.42seoul.kr>
2023-02-04 22:29:09 +0900

    [Project 1] Libft-2022/11/24

--- libft/libft/ft_putchar_fd.c
+++ libft/libft/ft_putchar_fd.c
@@ -6,13 +6,13 @@
 /*   By: hbyeon <hbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 04:46:11 by hbyeon            #+#    #+#             */
-/*   Updated: 2023/01/29 00:36:48 by hbyeon           ###   ########.fr       */
+/*   Updated: 2023/02/04 22:28:50 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd);
+void	ft_putchar_fd(char c, int fd)
 {
-	write(fd, &c[i], 1);
+	write(fd, &c, 1);
 }
