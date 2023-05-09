1b0d2cacb45ec5e86b6246d7c42d35e5853fc8a9
h-beeen <hbyeon@student.42seoul.kr>
2023-05-09 21:36:00 +0900

    [Project 5] minitalk-2023/05/09

--- minitalk/minitalk/client.c
+++ minitalk/minitalk/client.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbyeon <hbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/15 23:43:26 by hbyeon            #+#    #+#             */
-/*   Updated: 2023/05/03 12:44:37 by hbyeon           ###   ########.fr       */
+/*   Created: 2023/05/09 20:32:08 by hbyeon            #+#    #+#             */
+/*   Updated: 2023/05/09 20:46:25 by hbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "minitalk.h"
 
 int	ft_atoi(const char *str)
 {
@@ -39,4 +39,40 @@ int	ft_atoi(const char *str)
 		str++;
 	}
 	return (sign * result);
+}
+
+void	ft_atob(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(500);
+		bit++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc != 3)
+	{
+		write (1, "Error.", 6);
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	while (argv[2][i] != '\0')
+	{
+		ft_atob(pid, argv[2][i]);
+		i++;
+	}
+	return (0);
 }
---
+++ minitalk/minitalk/server.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbyeon <hbyeon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 20:32:58 by hbyeon            #+#    #+#             */
+/*   Updated: 2023/05/09 20:34:22 by hbyeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2", 2);
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+	}
+	if (nb < 10)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+void	ft_btoa(int sig)
+{
+	static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_putchar(i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	pid = getpid();
+	write(1, "[SERVER PID : ", 14);
+	ft_putnbr(pid);
+	write(1, "]\n\n", 3);
+	while (argc == 1)
+	{
+		signal(SIGUSR1, ft_btoa);
+		signal(SIGUSR2, ft_btoa);
+		pause();
+	}
+	return (0);
+}
